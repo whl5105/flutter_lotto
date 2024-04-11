36cf62fdbb6b6b49336c1c76f2157405ad592b5a
whl5105 <73993670+whl5105@users.noreply.github.com>
2024-04-11 20:17:20 +0900

    style : 스캔 영역 추가설정 및 화면 디자인 변경

--- lib/screens/qr_scanner_screen.dart
+++ lib/screens/qr_scanner_screen.dart
@@ -1,18 +1,31 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class QrScannerScreen extends StatefulWidget {
   const QrScannerScreen({Key? key}) : super(key: key);
 
   @override
-  State<QrScannerScreen> createState() => QrScannerScreennState();
+  State<QrScannerScreen> createState() => _QrScannerScreenState();
 }
 
-class QrScannerScreennState extends State<QrScannerScreen> {
+class _QrScannerScreenState extends State<QrScannerScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
   late QRViewController _controller;
-  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
   String scannedData = 'Scan a QR code';
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
 
   @override
   void dispose() {
@@ -22,49 +35,59 @@ class QrScannerScreennState extends State<QrScannerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('QR Code Scanner'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 5,
-              child: QRView(
-                key: _qrKey,
-                onQRViewCreated: _onQRViewCreated,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Code Scanner'),
+        elevation: 0,
+        foregroundColor: Colors.black,
+        backgroundColor: const Color(0xffF4F5F8),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.blue,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: MediaQuery.of(context).size.width * 0.8,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: Text(scannedData),
-              ),
+          ),
+          const Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Qr 코드 용지를 사각 안에 맞혀 스캔해 주세요'),
+              // child: (result != null)
+              //     ? Text(
+              //         'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+              //     : const Text('Qr 코드 용지를 사각 안에 맞혀 스캔해 주세요'),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
 
   void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      _controller = controller;
-      _controller.scannedDataStream.listen((scanData) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      if (result == null) {
         setState(() {
-          scannedData = 'Scanned data: ${scanData.code}';
-          _launchURL(scanData.code);
+          result = scanData;
         });
-      });
+        _launchURL(scanData.code);
+      }
     });
   }
+}
 
-  Future _launchURL(String? url) async {
-    if (url != null && await canLaunch(url)) {
-      await launch(url);
-    } else {
-      print('Could not launch $url');
-    }
+Future _launchURL(String? url) async {
+  if (url != null && await canLaunch(url)) {
+    await launch(url);
   }
 }
